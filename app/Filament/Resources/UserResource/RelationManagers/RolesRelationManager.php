1fbc3ce100ef3e432ad594d7a464872c583e91d2
Kristian Coulson <kristian.coulson@prettylittlething.com>
2023-05-22 13:44:05 +0100

    basic user management

--- app/Filament/Resources/UserResource/RelationManagers/RolesRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/RolesRelationManager.php
@@ -1,37 +1,29 @@
 <?php
 
-namespace App\Filament\Resources;
+namespace App\Filament\Resources\UserResource\RelationManagers;
 
-use App\Filament\Resources\PermissionResource\Pages;
-use App\Filament\Resources\PermissionResource\RelationManagers;
 use Filament\Forms;
-use Filament\Forms\Components\Card;
 use Filament\Resources\Form;
-use Filament\Resources\Resource;
+use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Spatie\Permission\Models\Permission;
 
-class PermissionResource extends Resource
+class RolesRelationManager extends RelationManager
 {
-    protected static ?string $model = Permission::class;
+    protected static string $relationship = 'roles';
 
-    protected static ?string $navigationIcon = 'heroicon-o-key';
-
-    protected static ?string $navigationGroup = 'User Management';
+    protected static ?string $recordTitleAttribute = 'name';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Card::make([
-                    Forms\Components\TextInput::make('name')
-                        ->autofocus()
-                        ->required(),
-                ]),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 
@@ -49,20 +41,15 @@ class PermissionResource extends Resource
             ->filters([
                 //
             ])
+            ->headerActions([
+                // Tables\Actions\CreateAction::make(),
+            ])
             ->actions([
-                // Tables\Actions\ViewAction::make(),
                 // Tables\Actions\EditAction::make(),
                 // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 // Tables\Actions\DeleteBulkAction::make(),
             ]);
-    }
-
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ManagePermissions::route('/'),
-        ];
     }
 }
