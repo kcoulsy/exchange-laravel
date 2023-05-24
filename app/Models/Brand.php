05d696c26e84c6484bb993bb344b1e84f76efab0
Kristian Coulson <kristian.coulson@prettylittlething.com>
2023-05-24 12:00:10 +0100

    Revert "feat: basic logging installed"
    This reverts commit 94886cdb8b0ab8812a0b9eb2b0ecbd2676acd563.

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,20 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Brand extends Model
 {
     use HasFactory;
-    use LogsActivity;
 
     protected $guarded = [];
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()
-            ->logOnly(['name', 'description']);
-        // Chain fluent methods for configuration options
-    }
 }
