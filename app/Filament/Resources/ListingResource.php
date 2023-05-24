fe924d99cbe7b44fbbec0e563f16c3c4a0262aa9
Kristian Coulson <kristian.coulson@prettylittlething.com>
2023-05-24 13:47:43 +0100

    feat: basic listing with media

---
+++ app/Filament/Resources/ListingResource.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ListingResource\Pages;
+use App\Filament\Resources\ListingResource\RelationManagers;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Condition;
+use App\Models\Currency;
+use App\Models\Listing;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class ListingResource extends Resource
+{
+    protected static ?string $model = Listing::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->label('User')
+                    ->required()
+                    ->options(User::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Forms\Components\Select::make('currency_id')
+                    ->label('Currency')
+                    ->preload()
+                    ->required()
+                    ->options(Currency::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Forms\Components\Select::make('category_id')
+                    ->label('Category')
+                    ->required()
+                    ->options(Category::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Forms\Components\Select::make('condition_id')
+                    ->label('Condition')
+                    ->preload()
+                    ->required()
+                    ->options(Condition::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Forms\Components\Select::make('brand_id')
+                    ->label('Brand')
+                    ->required()
+                    ->options(Brand::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn($state, callable $set) => $set('slug', Str::slug($state))),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('subtitle')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('model')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_por')
+                    ->label('Price on Request')
+                    ->required(),
+                Forms\Components\MarkdownEditor::make('description')
+                    ->fileAttachmentsDisk('admin-uploads')
+                    ->fileAttachmentsVisibility('public')
+                    ->required(),
+                Forms\Components\TextInput::make('location')
+                    ->required()
+                    ->maxLength(255),
+                SpatieMediaLibraryFileUpload::make('images')
+                    ->multiple()
+                    ->enableReordering(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('subtitle')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\IconColumn::make('is_por')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('location'),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListListings::route('/'),
+            'create' => Pages\CreateListing::route('/create'),
+            'edit' => Pages\EditListing::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2023_05_24_121140_create_listings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('price');
+            $table->boolean('is_por');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('model');
+            $table->string('location');
+            $table->foreignId('currency_id')->references('id')->on('currencies');
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->foreignId('condition_id')->references('id')->on('conditions');
+            $table->foreignId('brand_id')->references('id')->on('brands');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::table('listings', function (Blueprint $table) {
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
