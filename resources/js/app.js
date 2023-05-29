ac535b2875e4ef1c9c3d2d469738dd61203b8421
Kristian Coulson <kristian.coulson@prettylittlething.com>
2023-05-29 14:06:25 +0100

    fix: search that was removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
-import Alpine from 'alpinejs'
-import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
-import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
+import Alpine from "alpinejs";
+import FormsAlpinePlugin from "../../vendor/filament/forms/dist/module.esm";
+import NotificationsAlpinePlugin from "../../vendor/filament/notifications/dist/module.esm";
+import "./global-search";
 
-Alpine.plugin(FormsAlpinePlugin)
-Alpine.plugin(NotificationsAlpinePlugin)
+Alpine.plugin(FormsAlpinePlugin);
+Alpine.plugin(NotificationsAlpinePlugin);
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
-Alpine.start()
+Alpine.start();
---
+++ resources/js/global-search.js
@@ -0,0 +1,57 @@
+import autocomplete from "autocompleter";
+
+const input = document.getElementById("global-search");
+
+autocomplete({
+    input: input,
+    disableAutoSelect: true,
+    debounceWaitMs: 200,
+    fetch: function (text, update) {
+        fetch("/api/global-search?query=" + text).then(function (response) {
+            response.json().then(function (data) {
+                const searchData = [
+                    ...data.categories.map((category) => {
+                        return {
+                            label: category.name,
+                            value: `/${category.slug}`,
+                            group: "Categories",
+                        };
+                    }),
+                    ...data.listings.map((listing) => {
+                        return {
+                            label: listing.title,
+                            value: `/${listing.category.slug}/${listing.slug}`,
+                            group: "Listings",
+                        };
+                    }),
+                ];
+
+                update(searchData);
+            });
+        });
+    },
+    render: function (item, currentValue, ...args) {
+        const div = document.createElement("a");
+        div.textContent = item.label;
+        div.setAttribute("href", item.value);
+        div.classList.add(
+            "block",
+            "bg-gray-100",
+            "p-2",
+            "cursor-pointer",
+            "hover:bg-blue-200",
+            "group-[.selected]:bg-blue-200",
+            "aria-selected:bg-blue-200"
+        );
+        return div;
+    },
+    renderGroup: function (groupName, currentValue) {
+        var div = document.createElement("div");
+        div.textContent = groupName;
+        div.classList.add("bg-gray-200", "p-2", "border", "border-gray-100");
+        return div;
+    },
+    onSelect: function (item, ...x) {
+        window.location.href = item.value;
+    },
+});
