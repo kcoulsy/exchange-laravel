e75f09488abd430d64713fd906be46213e4f4deb
Kristian <kristiancoulson@gmail.com>
2023-06-12 19:33:23 +0100

    feat: add ms clarity

--- resources/js/app.js
+++ resources/js/app.js
@@ -45,6 +45,19 @@ function setupAnalytics() {
     gtag("js", new Date());
 
     gtag("config", "G-4T5JJW71G4");
+
+    (function (c, l, a, r, i, t, y) {
+        c[a] =
+            c[a] ||
+            function () {
+                (c[a].q = c[a].q || []).push(arguments);
+            };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "hiuv1wuxif");
 }
 
 if (Cookies.get("cookie_consent") === "all") {
