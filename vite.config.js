9a15c875d826fe571c57867b983f73512bfe9609
Kristian <kristiancoulson@gmail.com>
2023-07-15 17:12:25 +0100

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel, { refreshPaths } from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/alpine-standard.js",
+                "resources/js/alpine-filament.js",
+            ],
             refresh: [
                 ...refreshPaths,
                 "app/Http/Livewire/**",
